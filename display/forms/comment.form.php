7fe471f11d0876834e9c87dda2f745b42db977fa
Amanskywalker <ak47su30ac@gmail.com>
2016-01-20 22:56:27 +0530

    new file:   display/forms/comment.form.php

---
+++ display/forms/comment.form.php
@@ -0,0 +1,13 @@
+<?php
+	// comment.form.php
+?>
+
+<form>
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+    <textarea rows=1 class="mdl-textfield__input" id="comment"></textarea>
+    <label for="comment" class="mdl-textfield__label">Join the discussion</label>
+    </div>
+    <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon">
+    <i class="material-icons" role="presentation">check</i><span class="visuallyhidden">add comment</span>
+    </button>
+</form>
